be7e0d8d5d31866e673c91908171f5fe490b6dc3
ibrahim82-magsi <muhammad.ibrahim.magsi@gmail.com>
2025-09-27 17:52:31 +0500

    updated

---
+++ app.js
@@ -0,0 +1,26 @@
+// onclick
+
+// const  header = document.querySelector("#header") 
+
+// header.addEventListener("click" , ()=>{
+//     console.log("click event trigerd");
+    
+// })
+
+
+// onchange
+
+let form = document.querySelector("#input")
+
+form.addEventListener("input" , ()=>{
+    console.log("changed on trigerd");
+    
+})
+
+// onsubmit
+
+
+
+
+// onmouseout
+// onmouseover
